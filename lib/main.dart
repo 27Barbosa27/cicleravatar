af40db20d2f7e6cb10d0a30f8ed27f917d04d41c
27Barbosa27 <zedopipo49@gmail.com>
2024-05-06 12:22:51 +0100

    Update Scroll

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(appBar: AppBar(), body: HomeWidget()),
     );
   }
+}
 
 class HomeWidget extends StatelessWidget {
   const HomeWidget({super.key});
